38b7d2602af85021959f6b8471c66bb7cfbebca1
ruifffarinha-ops <ruifffarinha@gmail.com>
2026-09-23 09:22:32 +0100

    Atualizar RF Football para v8

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='rf-football-v7';
-const ASSETS=['./','index.html','styles.css?v=7','app.js?v=7','firebase-sync.js?v=7','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png','icons/icon-maskable-192.png','icons/icon-maskable-512.png','icons/apple-touch-icon.png','icons/favicon-32.png'];
+const CACHE='rf-football-v8';
+const ASSETS=['./','index.html','styles.css?v=8','app.js?v=8','firebase-sync.js?v=8','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png','icons/icon-maskable-192.png','icons/icon-maskable-512.png','icons/apple-touch-icon.png','icons/favicon-32.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
